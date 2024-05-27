a331746e8ffe1a3655b83dc4f9a1ad20a2d1b43a
NektorHealth <85933978+NektorHealth@users.noreply.github.com>
2024-05-27 07:07:23 +0530

    done

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 
 
+@app.route("/")
+def home():
+    return render_template("home.html",)
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
